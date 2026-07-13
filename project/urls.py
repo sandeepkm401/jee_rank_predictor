6031d571fa6a81c1a717df2d7804289117e7efbd
Sandeep Kommuru <sandeepkm401@gmail.com>
2026-07-13 18:52:56 +0530

    Necessary4

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.rankpredictor, name = "rankpredictor"),
+    path("rankpredictor/", views.rankpredictor, name = "rankpredictor"),
     path("working/", views.working, name = "working")
 ]
